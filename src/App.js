227565510802140decdf33e88fed1f6a69c1907f
Khaled Karim <kskarim177@gmail.com>
2022-05-22 01:08:57 +0600

    Loading Spinner added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import Navbar from './Pages/Shared/Header/Navbar';
+import NotFound from './Pages/Shared/NotFound/NotFound';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
-    <div>
+    <>
+      <Navbar />
       <Routes>
-        <Route>
-
-        </Route>
-
+        <Route path='/' element={<Home />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
       </Routes>
-    </div>
+    </>
   );
 }
 
